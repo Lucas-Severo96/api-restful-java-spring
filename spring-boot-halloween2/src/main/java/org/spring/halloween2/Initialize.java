5b0215917e66127bccd1cce552742713dcb81f53
Lucas Leite Severo <lucasleite_14.1@hotmail.com>
2025-08-02 22:28:07 -0300

    feat(JPA): Incluindo alterações para persistência de dados através das anotações JPA e dependência com o banco PostgreSQL

--- spring-boot-halloween2/src/main/java/org/spring/halloween2/Initialize.java
+++ spring-boot-halloween2/src/main/java/org/spring/halloween2/Initialize.java
@@ -1,6 +1,9 @@
 package org.spring.halloween2;
 
+import java.util.Random;
+
 import org.spring.halloween2.model.User;
+import org.spring.halloween2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,24 +12,41 @@ import org.springframework.stereotype.Component;
 public class Initialize implements CommandLineRunner {
 
     //Injetando dependência automática sempre que o tipo 'User' for referenciado
-    @Autowired
-    private User user01;
+    //@Autowired
+    private User user;
+
+    //@Autowired
+    //private User user02;
 
     @Autowired
-    private User user02;
+    UserRepository repository;
 
     //Método abstrato necessário devido o uso da interface CommandLineRunner
     @Override
     public void run(String... args) throws Exception {
-        user01.setId(1);
-        user01.setUsername("Lucas_Severo");
-        user01.setPassword("lucas123");
+        /*
+        user.setId(1);
+        user.setUsername("Lucas_Severo");
+        user.setPassword("lucas123");
 
         user02.setId(2);
         user02.setUsername("Diego_Severo");
         user02.setPassword("diego123");
 
-        System.out.println(user01 + "\n" + user02);
+        System.out.println(user + "\n" + user02);
+        */
+
+        Random gen = new Random();
+        for(int i = 0; i < 5 ; i++){
+            user = new User();
+            user.setUsername("Luc" + gen.nextLong(1000));
+            user.setPassword("Pass" + gen.nextLong(1000));
+            repository.save(user);
+        }
+
+        for(User u : repository.findAll()){
+            System.out.println(u);
+        }
         
     }
 
--- spring-boot-halloween2/src/main/java/org/spring/halloween2/model/User.java
+++ spring-boot-halloween2/src/main/java/org/spring/halloween2/model/User.java
@@ -3,12 +3,22 @@ package org.spring.halloween2.model;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Component
 @Data //Anotação do Lombok para criacao de Getters, Setters, toString(), hashcode() de forma implícita
 @Scope("prototype") //Substituir o escopo Padrão Singleton para evitar sobrescrição de objetos em memória
+@Entity
+@Table(name = "user_tb")
 public class User {
+    
+    @Id //Coluna id da tabela user_tb
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id; //Utilizando o "long" ao invés do "Long" para que seja possível usar o setId()
     private String username;
     private String password;
